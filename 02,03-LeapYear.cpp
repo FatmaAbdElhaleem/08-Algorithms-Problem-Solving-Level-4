4f41f5d63861a03eb5bc955fbed69bc1db9442f2
Fatma Abd Elhaleem <fatmaabdelhaleem22@gmail.com>
2026-05-10 22:42:53 +0300

    problem #2,3

---
+++ 02,03-LeapYear.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// Problem: #2/4
+// Write a program to check if year is a leap year or not.
+
+// Note:
+// All years which are perfectly divisible by 4 are leap years
+// except for century years (years ending with 00),
+// which are leap years only if they are perfectly divisible by 400.
+
+short readYear()
+{
+    short year;
+    cout << "Enter a year? ";
+    cin >> year;
+    return year;
+}
+
+bool isLeapYear(short year)
+{
+    // if (year % 4 != 0)
+    // {
+    //     return false;
+    // }
+    // if (year % 100 == 0 && year % 400 != 0)
+    // {
+    //     return false;
+    // }
+    // return true;
+    return (year % 400 == 0 || year % 4 == 0 && year % 100 != 0);
+}
+
+int main()
+{
+    short year = readYear();
+    if (isLeapYear(year))
+        cout << year << " is a leap year\n";
+    else
+        cout << year << " not a leap year\n";
+}
